2afab10d69b12d6137f1172f9a554e3a0b78b8fd
89few <2921888541@qq.com>
2026-07-09 16:14:40 +0800

    Add src/main/java/org/aiknowledgebase/dto/RegisterRequest.java

---
+++ src/main/java/org/aiknowledgebase/dto/RegisterRequest.java
@@ -0,0 +1,13 @@
+package org.aiknowledgebase.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    private String username;
+
+    private String password;
+
+    private String nickname;
+}
